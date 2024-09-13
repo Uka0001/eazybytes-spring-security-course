e4cd04d6acc7186e2a2d7fb2cb709e3840ec8441
oleksii.b <oleksii.b@dev.tagsoft.co>
2024-09-13 15:46:11 +0300

    added section 2 module

---
+++ springsecsection2/src/main/java/com/cryptos/springsecsection2/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.cryptos.springsecsection2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+    @GetMapping("/myContact")
+    public String getContactDetails() {
+        return "Here are the contact details from the DB";
+    }
+}
---
+++ springsecsection2/src/main/java/com/cryptos/springsecsection2/controller/NoticeController.java
@@ -0,0 +1,13 @@
+package com.cryptos.springsecsection2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticeController {
+
+    @GetMapping("/myNotice")
+    public String getNoticeDetails() {
+        return "Here are the notice details from the DB";
+    }
+}
