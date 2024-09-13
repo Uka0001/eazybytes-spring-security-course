cadb7626b900413ecf8cd1d39b439828411f493e
oleksii.b <oleksii.b@dev.tagsoft.co>
2024-09-13 22:42:44 +0300

    added sec 11

---
+++ springsecsection11/src/main/java/com/cryptos/springsecsection11/Springsecsection11Application.java
@@ -0,0 +1,13 @@
+package com.cryptos.springsecsection11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecsection11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springsecsection11Application.class, args);
+    }
+
+}
